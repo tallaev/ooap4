f5b975cc08ca49d7d41414f28d376fcc8347302c
tallaev <98256797+tallaev@users.noreply.github.com>
2024-05-14 10:00:38 +0700

    Add files via upload

---
+++ MudBlazorWebApp1/MudBlazorWebApp1/Components/Shared/Monitoring/Currency.cs
@@ -0,0 +1,28 @@
+﻿namespace MudBlazorWebApp1.Components.Shared.Monitoring
+{
+    public class Currency
+    {
+        private Random _rnd = new Random();
+        private DateTime DateTime = DateTime.Now;
+        public double Amount = 0;
+        private string Name = string.Empty;
+
+        public Currency(string name)
+        {
+            Name = name;
+        }
+
+        public double GetValueInRubles()
+        {
+            return Amount * 93;
+        }
+
+        public DateTime GetDateTime()
+        {
+            return DateTime;
+        }
+
+        public void SetAmount(double amount) { Amount = amount; }
+
+    }
+}
---
+++ MudBlazorWebApp1/MudBlazorWebApp1/Components/Shared/Monitoring/MoneyMonitoring.cs
@@ -0,0 +1,35 @@
+﻿using Microsoft.VisualBasic;
+using MudBlazor;
+
+
+namespace MudBlazorWebApp1.Components.Shared.Monitoring
+{
+    public class MoneyMonitoring
+    {
+        private Currency dollar = new("USD");
+        private Currency euro = new("EURO");
+
+        private Random _rnd = new Random();
+        ISnackbar _snackbar;
+        public MoneyMonitoring(ISnackbar snackbar)
+        {
+            _snackbar = snackbar;
+        }
+
+        public void StartMonitoring(List<ChartSeries> Series, List<string> Date)
+        {
+            euro.SetAmount((_rnd.Next(1500) / 10.0 + 100));
+            dollar.SetAmount((_rnd.Next(1500) / 10.0 + 100));
+            _snackbar.Add($"Взяли {euro.Amount} Евро; {dollar.Amount} долларов");
+
+            double dollarValue = dollar.GetValueInRubles();
+            double euroValue = euro.GetValueInRubles();
+            string dateAsStr = dollar.GetDateTime().ToString("yyyy-MM-dd HH-mm");
+            Date.Add(dateAsStr);
+
+            Series[0].Data = Series[0].Data.Append(dollarValue).ToArray();
+            Series[1].Data = Series[1].Data.Append(euroValue).ToArray();
+
+        }
+    }
+}
